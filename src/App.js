7a422b13ab4c2af75fde7b9f27161f722a57463d
Mohammad-Faisal <kiobostha300*#>
2021-10-17 20:52:10 +0600

    Demo complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BasicTable } from "./BasicTable";
+import { ActionTable } from "./ActionTable";
+import { CustomActionTable } from "./CustomActionTable";
+import { ImageTable } from "./ImageTable";
+import { ExportTable } from "./ExportTable";
+import { GroupTable } from "./GroupTable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BasicTable />
+            <ActionTable />
+            <CustomActionTable />
+            <ImageTable />
+            <ExportTable />
+            <GroupTable />
+        </div>
+    );
 }
 
 export default App;
